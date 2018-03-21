1a9df339ef5d4786d6b1883427d2fc6a7d081ce7
Martin Riedl <riedlmn@buntfalke1.ims.uni-stuttgart.de>
2018-03-21 10:13:35 +0100

    column.py script (similar to unix column) however, here the order of the split positions is considered

---
+++ column.py
@@ -0,0 +1,23 @@
+if len(sys.argv)<2:
+    print ("python col1,col2,col5,col2 file")
+    print ("or")
+    print ("cat file |python column.py col1,col2,col3")
+    sys.exit(0)
+cols = []
+for k in sys.argv[1].split(","):
+    cols.append(int(k))
+
+def nopen():
+    if len(sys.argv)>2:
+        return open(sys.argv[2])
+    return sys.stdin
+
+for l in nopen:
+    ls = l.strip().split()
+    line = ""
+    if len(l.strip())==0:
+        print (l.strip())
+        continue
+    for c in cols:
+        line+=" "+ls[c]
+    print (line[1:])
